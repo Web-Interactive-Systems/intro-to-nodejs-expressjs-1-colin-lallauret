e5ab46381b1d02059111b729a0c0374d7b5318f0
colin-lallauret <colinlallauret1@gmail.com>
2024-10-23 15:23:11 +0200

    lab2 completed

--- src/lab2.js
+++ src/lab2.js
@@ -15,3 +15,45 @@
   -- Hint, use postman to interact with the server 
   
  */
+const express = require("express");
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// GET request
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// POST request
+app.post("/", (req, res) => {
+  res.send("POST");
+});
+
+// PUT request
+app.put("/", (req, res) => {
+  res.send("PUT");
+});
+
+// DELETE request
+app.delete("/", (req, res) => {
+  res.send("DELETE");
+});
+
+// ALL request
+app.all("/all", (req, res) => {
+  res.send("ALL");
+});
+
+// Middleware example
+app.use((req, res, next) => {
+  console.log("Middleware");
+  next();
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
