03a414cb9972d0485c8a266c4f3ff27a441c4753
colin-lallauret <colinlallauret1@gmail.com>
2024-10-23 15:46:40 +0200

    lab3 completed

--- src/lab3.js
+++ src/lab3.js
@@ -5,3 +5,21 @@
  - Create a server that serves a static folder: `public`
  - If an url is not found, the server should serve 'Page not found'
  */
+
+const express = require("express");
+
+const PORT = 3000;
+
+const app = express();
+
+const staticMiddleware = express.static("public");
+
+app.use(staticMiddleware);
+
+app.use((req, res, next) => {
+  res.status(404).send("Page not found");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@
 // require("./lab0");
 
 // lab1
-require("./lab1");
+// require("./lab1");
+
+// lab2
+// require("./lab2");
+
+// lab3
+require("./lab3");
